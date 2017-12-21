4de7faac214d8f03b94a9f4c6eadf8e044bb8848
peixotte <peixotterafael@gmail.com>
2017-12-21 09:43:33 +0100

    Select UserType corrected

--- public/js/admin/administration.js
+++ public/js/admin/administration.js
@@ -7,19 +7,25 @@ function getUserDetails(id) {
 }
 
 function unlockUserEditMode() {
+    $("#usertype").removeAttr("disabled");
+    $('select').material_select();
     $('#newpassword-input').show();
     $('#confirmpassword-input').show();
     $('#user-edit-off-button').show();
     $('#user-edit-save-button').show();
     $('#user-edit-on-button').hide();
+
 }
 
 function lockUserEditMode() {
+    $("#usertype").prop("disabled", "disabled");
+    $('select').material_select();
     $('#newpassword-input').hide();
     $('#confirmpassword-input').hide();
     $('#user-edit-off-button').hide();
     $('#user-edit-save-button').hide();
     $('#user-edit-on-button').show();
+    $("#usertype").attr('disabled', 'disabled');
 }
 
 function newUserMode(){
